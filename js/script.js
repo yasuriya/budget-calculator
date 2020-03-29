8b9501d0db69216011fdf05a2659485d1211f494
Suri <anastasiia08@ukr.net>
2020-03-29 22:53:50 +0300

    functions added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-let money = +prompt("Your budget per month is? ", "");
-let time = +prompt("Type date YYYY-MM-DD");
+let money, time;
+
+function start() {
+  money = +prompt("Your budget per month is? ", "");
+  time = +prompt("Type date YYYY-MM-DD", "");
+
+  while (isNaN(money) || money == "" || money == null) {
+    money = +prompt("Your budget per month is? ", "");
+  }
+}
+
+start();
 
 let appData = {
   budget: money,
@@ -7,27 +17,74 @@ let appData = {
   expenses: {},
   optionalExpenses: {},
   income: [],
-  savings: false
+  savings: true
 };
 
-for (let i = 0; i < 2; i++) {
-  let a = prompt("Necessary position for buying", ""),
-    b = +prompt("How much does it cost?", "");
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Necessary position for buying", ""),
+      b = +prompt("How much does it cost?", "");
 
-  if (
-    typeof a === "string" &&
-    typeof a != null &&
-    typeof b != null &&
-    a != "" &&
-    b != "" &&
-    a.length < 50
-  ) {
-    console.log("done");
-    appData.expenses[a] = b;
-  } else {
-    i--;
+    if (
+      typeof a === "string" &&
+      typeof a != null &&
+      typeof b != null &&
+      a != "" &&
+      b != "" &&
+      a.length < 50
+    ) {
+      console.log("checked");
+      appData.expenses[a] = b;
+    } else {
+      i--;
+    }
   }
 }
+
+chooseExpenses();
+
+function detectDailyBudget() {
+  appData.moneyPerDay = (appData.budget / 30).toFixed();
+  alert("Daily budget is " + appData.moneyPerDay);
+}
+
+detectDailyBudget();
+
+function detectLevel() {
+  if (appData.moneyPerDay < 100) {
+    console.log("Minimal budget for life");
+  } else if (appData.moneyPerDay > 100 || appData.moneyPerDay < 2000) {
+    console.log("Middle budget");
+  } else if (appData.moneyPerDay > 2000) {
+    console.log("High budget");
+  } else {
+    console.log("UNKNOWN");
+  }
+}
+
+detectLevel();
+
+let save, percent;
+
+function checkSavings() {
+  if (appData.savings == true) {
+    save = +prompt("Money amount is:", "");
+    percent = +prompt("Monthly percent is: ", "");
+    appData.monthlyIncome = ((save / 100 / 12) * percent).toFixed(2);
+    alert("Your monthly income is: " + appData.monthlyIncome);
+  }
+}
+
+checkSavings();
+
+function chooseOptExpenses() {
+  for (let i = 1; i <= 3; i++) {
+    let q1 = prompt("Optional position for buying", "");
+    appData.optionalExpenses[i] = q1;
+    console.log(appData.optionalExpenses);
+  }
+}
+chooseOptExpenses();
 
 // let i = 0;
 // while (i < 2) {
@@ -68,18 +125,3 @@ for (let i = 0; i < 2; i++) {
 //     i--;
 //   }
 // } while (i < 2);
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Daily budget is " + appData.moneyPerDay);
-
-
-if (appData.moneyPerDay < 100) {
-  console.log("Minimal budget for life");
-} else if (appData.moneyPerDay > 100 || appData.moneyPerDay < 2000) {
-  console.log("Middle budget");
-} else if (appData.moneyPerDay > 2000) {
-  console.log("High budget");
-} else {
-  console.log("UNKNOWN");
-}

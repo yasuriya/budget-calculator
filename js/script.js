e61b27b1b7aadbf36ef37c3f5a45ef518dd2cca3
Suri <anastasiia08@ukr.net>
2020-03-29 17:52:18 +0300

    cycle & condition added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-let money = prompt("Your budget per month is? ", "");
-let time = prompt("Type date YYYY-MM-DD");
+let money = +prompt("Your budget per month is? ", "");
+let time = +prompt("Type date YYYY-MM-DD");
 
 let appData = {
   budget: money,
@@ -10,12 +10,76 @@ let appData = {
   savings: false
 };
 
-let q1 = prompt("Necessary position for buying", ""),
-  q2 = prompt("How much does it cost?", ""),
-  q3 = prompt("Necessary position for buying", ""),
-  q4 = prompt("How much does it cost?", "");
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Necessary position for buying", ""),
+    b = +prompt("How much does it cost?", "");
 
-appData.expenses[q1] = q2;
-appData.expenses[q3] = q4;
+  if (
+    typeof a === "string" &&
+    typeof a != null &&
+    typeof b != null &&
+    a != "" &&
+    b != "" &&
+    a.length < 50
+  ) {
+    console.log("done");
+    appData.expenses[a] = b;
+  } else {
+    i--;
+  }
+}
 
-alert(appData.budget / 30);
+// let i = 0;
+// while (i < 2) {
+//   let a = prompt("Necessary position for buying", ""),
+//     b = +prompt("How much does it cost?", "");
+//   i++;
+//   if (
+//     typeof a === "string" &&
+//     typeof a != null &&
+//     typeof b != null &&
+//     a != "" &&
+//     b != "" &&
+//     a.length < 50
+//   ) {
+//     console.log("done");
+//     appData.expenses[a] = b;
+//   } else {
+//     i--;
+//   }
+// }
+
+// let i = 0;
+// do {
+//   let a = prompt("Necessary position for buying", ""),
+//     b = +prompt("How much does it cost?", "");
+//   i++;
+//   if (
+//     typeof a === "string" &&
+//     typeof a != null &&
+//     typeof b != null &&
+//     a != "" &&
+//     b != "" &&
+//     a.length < 50
+//   ) {
+//     console.log("done");
+//     appData.expenses[a] = b;
+//   } else {
+//     i--;
+//   }
+// } while (i < 2);
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Daily budget is " + appData.moneyPerDay);
+
+
+if (appData.moneyPerDay < 100) {
+  console.log("Minimal budget for life");
+} else if (appData.moneyPerDay > 100 || appData.moneyPerDay < 2000) {
+  console.log("Middle budget");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("High budget");
+} else {
+  console.log("UNKNOWN");
+}
---
+++ js/test.js
@@ -0,0 +1,73 @@
+"use strict";
+
+// switch (browser) {
+//     case 'Edge':
+//       alert( "You've got the Edge!" );
+//       break;
+
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//       alert( 'Okay we support these browsers too' );
+//       break;
+
+//     default:
+//       alert( 'We hope that this page looks ok!' );
+//   }
+
+// let browser = prompt("What's your brows?");
+
+// if (browser == "Edge") {
+//   alert("You've got the Edge!");
+// } else if (browser == "Chrome" || "Firefox" || "Safari" || "Opera") {
+//   alert("Ok we support these brows too");
+// } else {
+//   alert("We hope...");
+// }
+
+// const number = +prompt("Введите число между 0 и 3", "");
+
+// if (number === 0) {
+//   alert('Вы ввели число 0');
+// }
+
+// if (number === 1) {
+//   alert('Вы ввели число 1');
+// }
+
+// if (number === 2 || number === 3) {
+//   alert('Вы ввели число 2, а может и 3');
+// };
+
+// switch (number) {
+//   case 0:
+//     alert("Zero");
+//     break;
+//   case 1:
+//     alert("Uno");
+//     break;
+//   case 2:
+//   case 3:
+//     alert("Two or three");
+//     break;
+//   default:
+//     alert("NaN");
+//     break;
+
+// }
+
+
+// switch (appData.moneyPerDay) {
+//   case appData.moneyPerDay < 100:
+//     console.log("Minimal budget for life");
+//     break;
+//   case appData.moneyPerDay > 100:
+//   case appData.moneyPerDay < 2000:
+//     console.log("Middle budget");
+//     break;
+//   case appData.moneyPerDay > 2000:
+//     console.log("High budget");
+//     break;
+// }
+
